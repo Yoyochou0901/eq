9008bd7201ac9a4784c3c1f8c9f37836f71d1ced
Yoyochou0901 <yoyochou0901@gmail.com>
2025-04-26 23:26:37 +0800

    fix: nav

--- script/nav.js
+++ script/nav.js
@@ -1,22 +1,30 @@
-function loadNav(depth = 0, now = -1) { 
+function loadNav(depth = 0, now = NaN) {
     let base = "";
     for (let i = 0; i < depth; i++) base += "../";
 
     const navItems = [
-        { name: "首頁", url: "index.html" },
-        { name: "災害地震清單", url: "disaster-quakes/index.html" },
-        { name: "斷層資訊", url: "faults/index.html" },
-        { name: "地震防災", url: "preparedness.html" },
-        { name: "科普", url: "info.html" },
-        { name: "相關網站", url: "sites.html" }
+        // { name: "首頁", url: "index.html", id: "index" },
+        { name: "災害地震清單", url: "disaster-quakes/index.html", id: "quake" },
+        { name: "斷層資訊", url: "faults/index.html", id: "fault" },
+        { name: "地震防災", url: "preparedness.html", id: "preparedness" },
+        { name: "科普", url: "science.html", id: "science" },
+        { name: "相關網站", url: "sites.html", id: "sites" }
     ];
 
-    const links = navItems.map((item, i) => {
-        const activeClass = (i === now) ? ' class="now"' : "";
-        return `<li><a href="${base}${item.url}"${activeClass}>${item.name}</a></li>`;
+    const links = navItems.map((item) => {
+        const activeClass = (item.id === now) ? ' class="now"' : "";
+        return `<li><a href="${base}${item.url}"${activeClass} id="${item.id}">${item.name}</a></li>`;
     }).join("");
 
-    const navHTML = `<nav><ul>${links}</ul></nav>`;
+
+    const navHTML = `
+    <nav>
+        <a href="${base}index.html" class="nav-home">
+            <img src="../resources/icon-home.svg" width=30px></i>
+        </a>
+        <ul>${links}</ul>
+    </nav>
+    `;
 
     document.body.insertAdjacentHTML("afterbegin", navHTML);
 }
