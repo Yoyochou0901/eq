51929487c34026df5027766e0d9c2562e3949eed
Yoyochou0901 <yoyochou0901@gmail.com>
2025-05-27 22:24:12 +0800

    fix: map

--- script/map.js
+++ script/map.js
@@ -1,13 +1,24 @@
 var map = L.map("map", {
   center: [23.5, 121],
   zoom: 8.25,
-  minZoom: 6,
-  maxZoom: 12,
+  minZoom: 7,
+  maxZoom: 20,
   zoomControl: false,
   attributionControl: false,
   zoomSnap: 0,
   updateWhenIdle: false,
 });
+
+
+function loadPhoto() {
+  L.tileLayer('https://wmts.nlsc.gov.tw/wmts/PHOTO2/default/GoogleMapsCompatible/{z}/{y}/{x}', {
+    maxNativeZoom: 20,
+    maxZoom: 20,
+    minZoom: 7
+  }).addTo(map);
+  console.info("Loaded World Map");
+  return;
+}
 
 function loadWorldMap() {
   return fetch("../resources/geodata/world.json")
@@ -55,9 +66,9 @@ function loadFaultData() {
       console.info("Loaded Fault Data");
     });
 }
-
 loadWorldMap()
-  .then(() => loadTaiwanMap())
+  // .then(() => loadTaiwanMap())
+  .then(() => loadPhoto())
   .then(() => loadFaultData())
   .catch((error) => console.error("載入失敗：", error));
 
