8ec52a44b1eca2e34c804d57773d08d241e77f0b
Yoyochou0901 <yoyochou0901@gmail.com>
2025-05-27 23:01:39 +0800

    fix: map

--- script/map.js
+++ script/map.js
@@ -9,23 +9,29 @@ var map = L.map("map", {
   updateWhenIdle: false,
 });
 
+let photoLayer = null;
+let taiwanLayer = null;
+let worldLayer = null;
+let isPhotoMode = true;
 
 function loadPhoto() {
-  L.tileLayer('https://wmts.nlsc.gov.tw/wmts/PHOTO2/default/GoogleMapsCompatible/{z}/{y}/{x}', {
+  photoLayer = L.tileLayer('https://wmts.nlsc.gov.tw/wmts/PHOTO2/default/GoogleMapsCompatible/{z}/{y}/{x}', {
     maxNativeZoom: 20,
     maxZoom: 20,
     minZoom: 7
   }).addTo(map);
-  console.info("Loaded World Map");
-  return;
+  console.info("Loaded Photo Layer");
 }
 
-function loadWorldMap() {
+function loadWorldMap(styleOptions) {
   return fetch("../resources/geodata/world.json")
     .then((response) => response.json())
     .then((geojsonData) => {
-      L.geoJSON(geojsonData, {
-        style: {
+      if (worldLayer) {
+        map.removeLayer(worldLayer);
+      }
+      worldLayer = L.geoJSON(geojsonData, {
+        style: styleOptions || {
           weight: 0,
           fillColor: "#353535",
           fillOpacity: 1,
@@ -39,7 +45,10 @@ function loadTaiwanMap() {
   return fetch("../resources/geodata/tw_area.geojson")
     .then((response) => response.json())
     .then((geojsonData) => {
-      L.geoJSON(geojsonData, {
+      if (taiwanLayer) {
+        map.removeLayer(taiwanLayer);
+      }
+      taiwanLayer = L.geoJSON(geojsonData, {
         style: {
           color: "#CCCCCC",
           weight: 1,
@@ -66,12 +75,71 @@ function loadFaultData() {
       console.info("Loaded Fault Data");
     });
 }
+
+function resetMapStyle() {
+  if (isPhotoMode) {
+    loadPhoto();
+    loadWorldMap({
+      color: "#AAAAAA",
+      weight: 2,
+      fillColor: "#000000",
+      fillOpacity: 0.3
+    });
+  } else {
+    loadTaiwanMap();
+    loadWorldMap({
+      weight: 0,
+      fillColor: "#353535",
+      fillOpacity: 1,
+    });
+  }
+}
+
+
+function toggleBaseLayer() {
+  isPhotoMode = !isPhotoMode;
+
+  if (photoLayer) {
+    map.removeLayer(photoLayer);
+    photoLayer = null;
+  }
+  if (taiwanLayer) {
+    map.removeLayer(taiwanLayer);
+    taiwanLayer = null;
+  }
+  resetMapStyle();
+}
+
 loadWorldMap()
-  // .then(() => loadTaiwanMap())
   .then(() => loadPhoto())
   .then(() => loadFaultData())
+  .then(() => resetMapStyle())
   .catch((error) => console.error("載入失敗：", error));
 
-L.control.zoom({
-  position: "bottomleft",
-}).addTo(map);
+L.Control.CustomControls = L.Control.extend({
+  onAdd: function () {
+    const container = L.DomUtil.create("div", "leaflet-bar leaflet-control-custom");
+
+    // Zoom in button
+    const zoomIn = L.DomUtil.create("button", "", container);
+    zoomIn.innerHTML = "+";
+    zoomIn.title = "放大";
+    zoomIn.onclick = () => map.zoomIn();
+
+    // Zoom out button
+    const zoomOut = L.DomUtil.create("button", "", container);
+    zoomOut.innerHTML = "−";
+    zoomOut.title = "縮小";
+    zoomOut.onclick = () => map.zoomOut();
+    return container;
+  },
+  onRemove: function () {
+    // Nothing to clean up here
+  },
+});
+
+L.control.customControls = function (opts) {
+  return new L.Control.CustomControls(opts);
+};
+
+L.control.customControls({ position: "topleft" }).addTo(map);
