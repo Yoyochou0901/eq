f79c02e4396537bc3d1d2a2b067db23322f57734
Yoyochou0901 <yoyochou0901@gmail.com>
2025-06-01 23:44:13 +0800

    fix:

--- script/science-section.js
+++ script/science-section.js
@@ -15,7 +15,7 @@ const items = [
         name: "海嘯", children: [
             { name: "海嘯是怎麼形成的", content: "海嘯的成因", img: "../resources/science/2-1_02.jpg", url: "2-1.html" },
             { name: "海嘯來前海水一定會後退嗎", content: "海嘯常見的迷思", img: "../resources/science/2-2_00.jpg", url: "2-2.html" },
-            { name: "1 公尺的海嘯也有危險嗎", content: "海嘯的破壞力", img: "", url: "2-3.html" },
+            { name: "1 公尺的海嘯也有危險嗎", content: "海嘯的破壞力", img: "../resources/science/2-3_00.png", url: "2-3.html" },
             { name: "台灣也會有海嘯嗎", content: "台灣過去的海嘯紀錄和未來可能發生的海嘯", img: "../resources/science/2-4_02.jpeg", url: "2-4.html" },
         ]
     },
