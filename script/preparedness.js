dfe20a15f3c59518fe00a8bb6bbe9730ce1c78b2
Yoyochou0901 <yoyochou0901@gmail.com>
2025-06-02 00:39:05 +0800

    fix:

--- script/preparedness.js
+++ script/preparedness.js
@@ -2,7 +2,7 @@ const items = [
     { name: "地震前該做的事", content: "地震來臨前，平時可以做的防震準備", badge: "地震", url: "#" },
     { name: "地震發生時", content: "地震發生當下，該如何應變", badge: "地震", url: "#" },
     { name: "地震後", content: "地震發生後，該注意的事", badge: "地震", url: "#" },
-    { name: "海嘯防護", content: "海嘯發生時，如何避難", badge: "海嘯", url: "#" },
+    { name: "海嘯防災", content: "海嘯發生時，如何應對", badge: "海嘯", url: "4.html" },
 ]
 const preparednessSection = document.getElementById("preparedness-section")
 
