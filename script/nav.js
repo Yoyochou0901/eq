5fdc3b3f60a2c1e89f49359facae35f0606a78d6
Yoyochou0901 <yoyochou0901@gmail.com>
2025-06-01 21:25:25 +0800

    fix: content

--- script/nav.js
+++ script/nav.js
@@ -5,7 +5,7 @@ function loadNav(depth = 0, now = NaN) {
     const navItems = [
         // { name: "首頁", url: "index.html", id: "index" },
         { name: "災害地震清單", url: "disaster-quakes/", id: "quake" },
-        { name: "斷層資訊", url: "faults/", id: "fault" },
+        { name: "斷層分布", url: "faults/", id: "fault" },
         { name: "防災知識", url: "preparedness/", id: "preparedness" },
         { name: "科普", url: "science/", id: "science" },
         { name: "相關網站", url: "sites.html", id: "sites" }
